2674f8ef19054128f8f734ecd3109f61f7a57127
jhernandez9401 <90942695+jhernandez9401@users.noreply.github.com>
2021-10-13 20:31:34 -0500

    Modulo de Reportes
    Se sube el modulo de reportes solo falta conectar con el de ventas

--- tienda53/src/test/java/com/grupo53/tienda53/BO/TiendaApplicationTests.java
+++ tienda53/src/test/java/com/grupo53/tienda53/BO/TiendaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.grupo53.tienda53;
+package com.grupo53.tienda53.BO;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Tienda53ApplicationTests {
+class TiendaApplicationTests {
 
 	@Test
 	void contextLoads() {
